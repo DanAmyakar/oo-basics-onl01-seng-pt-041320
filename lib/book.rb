0b6a48201d343ede23210755048f187c41da5b9c
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-27 19:52:29 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,5 +7,21 @@ class Book
     @title
   end
   
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
 end
 
